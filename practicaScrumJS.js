9209675e18ca81bcd91e6b48cb8fdb7eefb91bc6
Itzel <itzelrolon2001@gmail.com>
2025-12-17 13:22:59 -0600

    Cambios archivo java

---
+++ practicaScrumJS.js
@@ -0,0 +1,8 @@
+alert("Bienvenido a Nintendo Gamers");
+console.log(pokemones.length);
+//arreglo con personajes de pokemon
+let pokemones = ["Caterpie", "Charmander", "Venusaur", "Butterfree"];
+
+//ejemplo imaginario
+let elegido = pokemones[1];
+console.log("Has elegido a " + elegido + " ¡Conoce su informacion!");
